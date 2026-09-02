79a921bd88b98e88df054e087ea836eb35df972d
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-09-02 10:37:36 +0200

    Create check.py

---
+++ check.py
@@ -0,0 +1,47 @@
+import json
+from pathlib import Path
+
+
+MIN_VERSION = "0.3.14"
+
+# Ordner, in dem dieses Script liegt
+folder = Path(__file__).parent
+folder = Path(str(folder) + "/templates")
+
+for json_file in folder.glob("*.json5"):
+    try:
+        with json_file.open("r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        # Nur ergänzen, wenn min_version nicht vorhanden ist
+        if "min_version" not in data:
+            data["min_version"] = MIN_VERSION
+
+            with json_file.open("w", encoding="utf-8") as f:
+                json.dump(data, f, indent=4, ensure_ascii=False)
+                f.write("\n")
+
+            print(f"Ergänzt: {json_file.name}")
+        else:
+            #print(f"Bereits vorhanden: {json_file.name}")
+            pass
+
+    except json.JSONDecodeError:
+        print(f"Übersprungen (ungültiges JSON): {json_file.name}")
+    except Exception as e:
+        print(f"Fehler bei {json_file.name}: {e}")
+
+
+for json_file in folder.glob("*.json5"):
+    try:
+        with json_file.open("r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        # Nur ergänzen, wenn min_version nicht vorhanden ist
+        if "tags" not in data:
+            print(f"No Tags: {json_file.name}")
+
+    except json.JSONDecodeError:
+        print(f"Übersprungen (ungültiges JSON): {json_file.name}")
+    except Exception as e:
+        print(f"Fehler bei {json_file.name}: {e}")
