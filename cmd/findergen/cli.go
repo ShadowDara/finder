0c34c57589302005e9659d68ddc4514715417bcb
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-09-04 14:56:52 +0200

    Update cli.go

--- cmd/findergen/cli.go
+++ cmd/findergen/cli.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/shadowdara/finder/internal/finderversion"
 	"github.com/shadowdara/finder/pub/argparser"
 )
 
@@ -19,13 +21,26 @@ func parseCliArgs() Conf {
 	root := argparser.NewCommand("findergen",
 		"the http server for finder to create and view templates", false)
 
+	// Add Version Command
+	versionCmd := argparser.NewCommand(
+		"--version", "to get the Version of the Program", false, "-v", "v", "version")
+
 	// Port
 	root.Number("port", 0, "Change the Server Port", false, "p")
+
+	root.AddSubcommand(versionCmd)
 
 	// Parse the Arguments
 	cmd := root.Parse(os.Args[1:])
 
 	newconf.port = int(cmd.GetNumber("port"))
+
+	switch cmd {
+	case versionCmd:
+		{
+			fmt.Println(finderversion.Version)
+		}
+	}
 
 	return newconf
 }
