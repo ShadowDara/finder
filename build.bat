d916de5dfd019a5e3e0765cbf48153a21a24c4ed
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-09-04 15:46:58 +0200

    continue on 2nd branch

--- build.bat
+++ build.bat
@@ -3,8 +3,6 @@
 go build -ldflags="-s -w" ./cmd/finder
 go build -ldflags="-s -w" ./cmd/findergen
 go build -ldflags="-s -w" ./cmd/tester
-go build -ldflags="-s -w" ./glua/cmd/bt
-go build -ldflags="-s -w" ./glua/cmd/glua
 
 for %%F in ("finder.exe") do echo Größe: %%~zF Bytes
 for %%F in ("findergen.exe") do echo Größe: %%~zF Bytes
--- glua/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/shadowdara/finder/glua
-
-go 1.23
-
-toolchain go1.24.4
-
-require github.com/yuin/gopher-lua v1.1.2 // indirect
