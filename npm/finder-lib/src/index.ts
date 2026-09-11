fb4f9e7e24d0c716ac8142c34fcf41e25e18bb65
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-09-11 14:20:12 +0200

    Update index.ts

--- npm/finder-lib/src/index.ts
+++ npm/finder-lib/src/index.ts
@@ -44,88 +44,213 @@ export interface Checksums {
 }
 
 // ALL Versions
+export namespace versions {
+  // Version v0.0.0
+  export namespace v0_0_0 {
+    export type template = Folder;
 
-export namespace v0_3_16 {
-  export interface Template {
-    min_version?: string;
-    description?: string;
-    tags?: string[];
-    name: string;
-    folders?: Folder[];
-    files?: File[];
-    command?: string;
-    invert_command?: boolean;
-    size?: Size;
+    export interface Folder {
+      name: string;
+      folders?: Folder[];
+      files?: string[];
+    }
   }
 
-  export interface Folder {
-    name: string;
-    folders?: Folder[];
-    files?: File[];
-    command?: string;
-    invert_command?: boolean;
-    size?: Size;
+  // Version v0.3.1
+  export namespace v0_3_1 {
+    export interface Template {
+      description?: string;
+      tags?: string[];
+      name: string;
+      folders?: Folder[];
+      files?: string[];
+      command?: string;
+      invert_command?: boolean;
+    }
+
+    export interface Folder {
+      name: string;
+      folders?: Folder[];
+      files?: File[];
+      command?: string;
+      invert_command?: boolean;
+    }
   }
 
-  export interface File {
-    name: string;
-    existence?: string;
-    size?: Size;
-    checksums?: Checksums;
+  // Version v0.3.3
+  export namespace v0_3_3 {
+    export interface Template {
+      description?: string;
+      tags?: string[];
+      name: string;
+      folders?: Folder[];
+      files?: string[] | File[];
+      command?: string;
+      invert_command?: boolean;
+    }
+
+    export interface Folder {
+      name: string;
+      folders?: Folder[];
+      files?: File[] | string[];
+      command?: string;
+      invert_command?: boolean;
+    }
+
+    export interface File {
+      name?: string;
+      existence?: string;
+    }
   }
 
-  export interface Size {
-    mix?: number;
-    max?: number;
-    min_size_type?: string;
-    max_size_type?: string;
+  // Version v0.3.6
+  export namespace v0_3_6 {
+    export interface Template {
+      min_version?: string;
+      description?: string;
+      tags?: string[];
+      name: string;
+      folders?: Folder[];
+      files?: File[] | string[];
+      command?: string;
+      invert_command?: boolean;
+    }
+
+    export interface Folder {
+      name: string;
+      folders?: Folder[];
+      files?: File[] | string[];
+      command?: string;
+      invert_command?: boolean;
+    }
+
+    export interface File {
+      name: string;
+      existence?: string;
+    }
   }
 
-  export interface Checksums {
-    sha256?: string;
-    sha512?: string;
-  }
-}
+  // Version v0.3.15
+  export namespace v0_3_15 {
+    export interface Template {
+      min_version?: string;
+      description?: string;
+      tags?: string[];
+      name: string;
+      folders?: Folder[];
+      files?: File[] | string[];
+      command?: string;
+      invert_command?: boolean;
+      size?: Size;
+    }
 
-export namespace v0_3_17 {
-  export interface Template {
-    min_version?: string;
-    description?: string;
-    tags?: string[];
-    name: string;
-    folders?: Folder[];
-    files?: File[];
-    command?: string;
-    invert_command?: boolean;
-    size?: Size;
-    mdnote?: string;
-  }
+    export interface Folder {
+      name: string;
+      folders?: Folder[];
+      files?: File[] | string[];
+      command?: string;
+      invert_command?: boolean;
+      size?: Size;
+    }
 
-  export interface Folder {
-    name: string;
-    folders?: Folder[];
-    files?: File[];
-    command?: string;
-    invert_command?: boolean;
-    size?: Size;
-  }
+    export interface File {
+      name: string;
+      existence?: string;
+      size?: Size;
+    }
 
-  export interface File {
-    name: string;
-    existence?: string;
-    size?: Size;
-    checksums?: Checksums;
-  }
-
-  export interface Size {
-    mix?: number;
-    max?: number;
-    min_size_type?: string;
-    max_size_type?: string;
+    export interface Size {
+      mix?: number;
+      max?: number;
+      min_size_type?: string;
+      max_size_type?: string;
+    }
   }
 
-  export interface Checksums {
-    sha256?: string;
-    sha512?: string;
+  // Version v0.3.16
+  export namespace v0_3_16 {
+    export interface Template {
+      min_version?: string;
+      description?: string;
+      tags?: string[];
+      name: string;
+      folders?: Folder[];
+      files?: File[] | string[];
+      command?: string;
+      invert_command?: boolean;
+      size?: Size;
+    }
+
+    export interface Folder {
+      name: string;
+      folders?: Folder[];
+      files?: File[] | string[];
+      command?: string;
+      invert_command?: boolean;
+      size?: Size;
+    }
+
+    export interface File {
+      name: string;
+      existence?: string;
+      size?: Size;
+      checksums?: Checksums;
+    }
+
+    export interface Size {
+      mix?: number;
+      max?: number;
+      min_size_type?: string;
+      max_size_type?: string;
+    }
+
+    export interface Checksums {
+      sha256?: string;
+      sha512?: string;
+    }
+  }
+
+  // Version v0.3.17
+  export namespace v0_3_17 {
+    export interface Template {
+      min_version?: string;
+      description?: string;
+      tags?: string[];
+      name: string;
+      folders?: Folder[];
+      files?: File[] | string[];
+      command?: string;
+      invert_command?: boolean;
+      size?: Size;
+      mdnote?: string;
+    }
+
+    export interface Folder {
+      name: string;
+      folders?: Folder[];
+      files?: File[] | string[];
+      command?: string;
+      invert_command?: boolean;
+      size?: Size;
+    }
+
+    export interface File {
+      name: string;
+      existence?: string;
+      size?: Size;
+      checksums?: Checksums;
+    }
+
+    export interface Size {
+      mix?: number;
+      max?: number;
+      min_size_type?: string;
+      max_size_type?: string;
+    }
+
+    export interface Checksums {
+      sha256?: string;
+      sha512?: string;
+    }
   }
 }
