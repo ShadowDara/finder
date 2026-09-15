37fc2ffa6d237f07cdd019101d3c62132cb2cb22
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-09-15 13:32:48 +0200

    Move checkstaticfiles to pub package
    This change relocates the checkstaticfiles project under pub/checkstaticfiles, updates the Go package name to checkstaticfiles, and fixes the import path to the finder module. It also keeps the existing functionality intact while cleaning up minor formatting in the check function.

--- .github/temp-stuff-idk/checkstaticfiles-main/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/shadowdara/checkstaticfiles
-
-go 1.24.2
--- pub/checkstaticfiles/checkstaticfiles.go
+++ pub/checkstaticfiles/checkstaticfiles.go
@@ -1,22 +1,22 @@
-package shadowdara_checkstaticfiles
+package checkstaticfiles
 
 import (
 	"log"
 
-	"github.com/shadowdara/checkstaticfiles/core"
+	"github.com/shadowdara/finder/pub/checkstaticfiles/core"
 )
 
 func Checkfiles(data []byte, settings int) {
 	log.Println("Checking required files...")
 	core.Main(data, settings)
-	
+
 	// Function is not ready yet
 	//check_gitginore(settings)
 }
 
 // option to create an .gitignore file
 func check_gitginore(settings int) {
-	 if (core.Checksettings(settings, 0)) {
+	if core.Checksettings(settings, 0) {
 		// Check if gitignore file exists
-	 }
+	}
 }
