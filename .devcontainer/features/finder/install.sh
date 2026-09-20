374939cd0f94fafa2d5490f1f827a0fe85a1372d
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-09-20 02:18:43 +0200

    Update install.sh

--- .devcontainer/features/finder/install.sh
+++ .devcontainer/features/finder/install.sh
@@ -3,5 +3,5 @@ set -e
 
 echo "Installing finder Version v0.3.18 ..."
 
-# curl -fsSL https://raw.githubusercontent.com/ShadowDara/finder/refs/heads/main/install.sh -y --version v0.3.18 | sh
+# curl -fsSL https://raw.githubusercontent.com/ShadowDara/finder/refs/heads/main/install.sh -y --version v0.3.18 --type default | sh
 curl -fsSL https://raw.githubusercontent.com/ShadowDara/finder/refs/heads/main/install.sh -y --latest | sh
