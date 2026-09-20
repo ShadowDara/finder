49c7b4590deaa81bf77eaad303d350ab4b2951e9
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-09-20 14:24:32 +0200

    fix d tool

--- cmd/dih/dih-helper.c
+++ cmd/dih/dih-helper.c
@@ -4,24 +4,23 @@ int handle_init() { printf("init\n"); }
 
 int handle_build() { printf("build\n"); }
 
-int handle_run(int argc, char **argv)
-{
-    printf("run\n");
-    for (size_t i = 0; i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
+int handle_run(int argc, const char **argv) {
+  printf("run\n");
+  for (size_t i = 0; i < argc; i++) {
+    printf("%s\n", argv[i]);
+  }
 }
 
-int handle_help()
-{
-    printf("Help\n"
-           "This is the help message for dih, a small C cli tool\n"
-           "lol\n");
+int handle_help() {
+  printf("Help\n"
+         "This is the help message for dih, a small C cli tool\n"
+         "lol\n");
 }
 
-int handle_cmo(int argc, char **argv) { return cmo_main(); }
+int handle_cmo(int argc, const char **argv) { return cmo_main(); }
 
-int handle_tagp(int argc, char **argv) { return tag_push_main(argc, argv); }
+int handle_tagp(int argc, const char **argv) {
+  return tag_push_main(argc, argv);
+}
 
-int handle_ign(int argc, char **argv) { return ign_main(argc, argv); }
+int handle_ign(int argc, const char **argv) { return ign_main(argc, argv); }
--- cmd/dih/dih-helper.h
+++ cmd/dih/dih-helper.h
@@ -8,15 +8,15 @@ int handle_init();
 
 int handle_build();
 
-int handle_run(int argc, char **argv);
+int handle_run(int argc, const char **argv);
 
 int handle_help();
 
-int handle_cmo(int argc, char **argv);
+int handle_cmo(int argc, const char **argv);
 
-int handle_tagp(int argc, char **argv);
+int handle_tagp(int argc, const char **argv);
 
-int handle_ign(int argc, char **argv);
+int handle_ign(int argc, const char **argv);
 
 //  /$$$$$$$  /$$$$$$ /$$   /$$
 // | $$__  $$|_  $$_/| $$  | $$
